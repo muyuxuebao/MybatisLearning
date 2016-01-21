53fc94be0b1a3b54637942a5de75d93306fe9ed7
muyux <muyuxuebao@163.com>
2016-01-21 10:33:16 +0800

    first commit

--- src/main/java/cn/itcast/mybatis/first/MybatisFirst.java
+++ src/main/java/cn/itcast/mybatis/first/MybatisFirst.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -74,7 +75,27 @@ public class MybatisFirst {
     // 添加用户信息
     @Test
     public void insertUserTest() throws IOException {
+        // mybatis配置文件
+        String resource = "SqlMapConfig.xml";
+        // 得到配置文件流
+        InputStream inputStream = Resources.getResourceAsStream(resource);
 
+        // 创建会话工厂，传入mybatis的配置文件信息
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        // 通过工厂得到SqlSession
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        // 插入用户对象
+        User user = new User();
+        user.setUsername("王小军");
+        user.setBirthday(new Date());
+        user.setSex("1");
+        user.setAddress("河南郑州");
+
+
+        // 释放资源
+        sqlSession.close();
     }
 
 }
